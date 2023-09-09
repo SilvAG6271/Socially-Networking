fa0eb90b80528706b95d20e828b1d1454585c8c9
auron <auron7985@gmail.com>
2023-09-09 12:37:25 -0500

    added User and Thought models

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const Thought = require('./Thought');
+
+const userSchema = new Schema(
+    {
+username: {
+    type: String,
+    required: true, 
+    unique: true, 
+    trim: true},
+email: {
+    type: String, 
+    required: true, 
+    unique: true,
+    validator: function(v) {
+        return /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2-4\b/.test(v)
+    },
+    message: "Invalid email format",
+},
+
+thoughts: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: 'Thought',
+},
+],
+friends: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+ ],
+},
+{
+toJSON: {
+    virtuals: true,
+},
+id: false,
+ }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+module.exports = User;
